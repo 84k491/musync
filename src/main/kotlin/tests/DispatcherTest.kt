2a64eadf7c0311161007bec5c2ea6403e5f76206
Sviatoslav Bakaras <bakar1337@gmail.com>
2023-03-18 19:00:34 +0400

    Add test

---
+++ src/main/kotlin/tests/DispatcherTest.kt
@@ -0,0 +1,11 @@
+package tests
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DispatcherTest {
+    @Test
+    fun exampleTest() {
+        assertEquals(1,1)
+    }
+}
